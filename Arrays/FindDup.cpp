7c96d62007f02d1b27fa4077224eba3258325be9
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2024-07-15 01:42:24 -0400

    Added sol

---
+++ Arrays/FindDup.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            if(nums[abs(nums[i])]<0) return abs(nums[i]);
+            nums[abs(nums[i])] *= -1;
+        }
+        return -1;
+    }
+};
